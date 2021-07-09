e0604827532bb1a4bd712f665dcf3ebb0a410fc5
Joshua Horner <lesliewbusiness@gmail.com>
2021-07-09 14:17:15 -0600

    Finish book test suite

--- src/book.js
+++ src/book.js
@@ -1,20 +1,48 @@
-function createTitle(title) {
-  return `The ${title}`;
+function createTitle(bookTitle) {
+  return `The ${bookTitle}`;
 };
 
 function buildMainCharacter(name, age, pronouns) {
-  return {name, age, pronouns};
+  var character = {
+    name: name,
+    age: age,
+    pronouns: pronouns
+  }
+  return character;
 };
 
-function saveReview() {
+function saveReview(review, reviews) {
+  if (reviews.includes(review)) {
+    return;
+  } else {
+    reviews.push(review);
+}
+};
 
+function calculatePageCount(bookTitle) {
+  return bookTitle.length * 20;
+};
+
+function writeBook(bookTitle, bookCharacter, genre) {
+  var bookDetails = {
+    title: bookTitle,
+    mainCharacter: bookCharacter,
+    pageCount: calculatePageCount(bookTitle),
+    genre: genre,
+  };
+  return bookDetails;
+};
+
+function editBook(book) {
+  book.pageCount = book.pageCount * .75;
+  return book;
 };
 
 module.exports = {
   createTitle,
   buildMainCharacter,
   saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  calculatePageCount,
+  writeBook,
+  editBook
 }
