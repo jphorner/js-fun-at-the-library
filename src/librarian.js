54da3b99bc03d8c0096d2da8daed150f7392ed73
Joshua Horner <lesliewbusiness@gmail.com>
2021-07-11 16:25:37 -0600

    Finish Librarian test suite

--- src/librarian.js
+++ src/librarian.js
@@ -1,7 +1,37 @@
 class Librarian {
-  constructor() {
-    
+  constructor(name, library) {
+    this.name = name;
+    this.library = library;
+  };
+  greetPatron(name, isMorning) {
+    if (isMorning === true) {
+      return `Good morning, ${name}!`;
+    }
+    return `Hello, ${name}!`;
+  };
+  findBook(book) {
+    var genreFantasy = this.library.shelves.fantasy;
+    var genreFiction = this.library.shelves.fiction;
+    var genreNonFiction = this.library.shelves.nonFiction;
+    var confirmationMessage = `Yes, we have ${book}`;
+    for (var i = 0; i < 3; i++) {
+      if (genreFantasy[i] && genreFantasy[i].title === book) {
+        genreFantasy.splice(i, 1);
+        return confirmationMessage;
+      } else if (genreFiction[i] && genreFiction[i].title === book) {
+        genreFiction.splice(i, 1);
+        return confirmationMessage;
+      } else if (genreNonFiction[i] && genreNonFiction[i].title === book) {
+        genreNonFiction.splice(i, 1);
+        return confirmationMessage;
+      };
+      return `Sorry, we do not have ${book}`;
+    }
+  };
+  calculateLateFee(number) {
+    var lateFee = Math.ceil(number * .25);
+    return lateFee;
   }
-}
+};
 
 module.exports = Librarian;
